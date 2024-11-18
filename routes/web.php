ddedb5c18a4ea563aaa9d9ed729a8c0924f0fce0
Jairo Becerra <jairobecerra03@gmail.com>
2024-11-17 19:30:12 -0500

    Se crean y definen Controllers y se añaden sus rutas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,23 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\PizzaController;
+use App\Http\Controllers\PizzaSizeController;
+use App\Http\Controllers\IngredientController;
+use App\Http\Controllers\PizzaIngredientController;
+use App\Http\Controllers\ExtraIngredientController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderPizzaController;
+use App\Http\Controllers\OrderExtraIngredientController;
+use App\Http\Controllers\BranchController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\RawMaterialController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\PizzaRawMaterialController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -30,6 +47,25 @@ Route::middleware('auth')->prefix('user')->name('user.')->group(function () {
     Route::get('pizzas', [UserController::class, 'showPizzas'])->name('pizzas.index');
     Route::post('orders', [UserController::class, 'orderPizza'])->name('orders.store');
     Route::get('orders', [UserController::class, 'showOrderStatus'])->name('orders.index');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('users', UserController::class);
+    Route::resource('clients', ClientController::class);
+    Route::resource('employees', EmployeeController::class);
+    Route::resource('pizzas', PizzaController::class);
+    Route::resource('pizza-sizes', PizzaSizeController::class);
+    Route::resource('ingredients', IngredientController::class);
+    Route::resource('pizza-ingredients', PizzaIngredientController::class);
+    Route::resource('extra-ingredients', ExtraIngredientController::class);
+    Route::resource('orders', OrderController::class);
+    Route::resource('order-pizzas', OrderPizzaController::class);
+    Route::resource('order-extra-ingredients', OrderExtraIngredientController::class);
+    Route::resource('branches', BranchController::class);
+    Route::resource('suppliers', SupplierController::class);
+    Route::resource('raw-materials', RawMaterialController::class);
+    Route::resource('purchases', PurchaseController::class);
+    Route::resource('pizza-raw-materials', PizzaRawMaterialController::class);
 });
 
 
